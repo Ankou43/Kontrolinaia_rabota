0af15757b563796ac48fdf17d77b22e415a63f69
Artur Frolov <art.frolov2479@gmail.com>
2023-11-23 13:09:20 +0200

    Написал программу, проверил

--- rabota_1/Program.cs
+++ rabota_1/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿static string[] FilterStrings(string[] array)
+{
+    // Тут я сделал фильтр для слов <=3, count выполняет роль счетчика для нового массива. Trim избавляет нас от лишних пробелов
+    int count = 0;
+    foreach (var item in array)
+    {
+        if (item.Trim().Length <= 3)
+        {
+            count++;
+        }
+    }
+
+    // Создание нового массива нужного размера
+    string[] resultArray = new string[count];
+
+    // Заполнение нового массива
+    int index = 0;
+    foreach (var item in array)
+    {
+        if (item.Trim().Length <= 3)
+        {
+            resultArray[index] = item.Trim();
+            index++;
+        }
+    }
+
+    return resultArray;
+}
+
+Console.WriteLine("Введите элементы массива строк, разделенные запятой:");
+
+// Тут вы будете ввести ваш массив в терминале
+string inputString = Console.ReadLine();
+
+// Разбивка строки на элементы массива
+string[] originalArray = inputString.Split(',');
+
+// Создание нового массива
+string[] newArray = FilterStrings(originalArray);
+
+// Вывод нового массива
+Console.WriteLine("Новый массив: [" + string.Join(", ", newArray) + "]");
